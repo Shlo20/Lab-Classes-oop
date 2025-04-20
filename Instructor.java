f6a68d026aa72e5592da9f6d4c5e2e8b142015ef
Shlomo Ovadya <shlomovadya041@gmail.com>
2025-04-20 03:16:58 -0400

    init comit

--- Instructor.java
+++ Instructor.java
@@ -1,12 +1,17 @@
 
 /**
- * Write a description of class Instructor here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * The Instructor class represents a lab instructor and extends the Person class.
+ * It inherits the instructor's name and contact information from the Person superclass.
+ * This class overrides the print and toString methods for custom output.
+
+ * @author Shlomo
+ * @version 2025.04.20
  */
+
 public class Instructor extends Person
 {
+
+    //Constructs a new Instructor with the specified name and contact details
     public Instructor(String name, String contact)
     {
         super(name, contact);
@@ -17,7 +22,8 @@ public class Instructor extends Person
     {
         System.out.println("Instructor: " + getName() + ", Contact: " + getContact());
     }
-    
+
+    //Returns a string representation of the instructor using only their name.
     @Override
     public String toString() {
         return getName(); 
